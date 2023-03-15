48adc00ae88f6d44908aa0abececf0b2e0638ca5
Alexandre <alexandre.sautet@protonmail.com>
2023-03-15 13:25:35 +0100

    Ajout doc v4

--- v4_ecran/v4_ecran.ino
+++ v4_ecran/v4_ecran.ino
@@ -14,15 +14,14 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 /**
 *Calcul de la tension d'entre a partir des resistances et Vout
 */
-float calculVin(float tensionVOUT){
+float calculVin(float tensionVOUT) {
 
   float tensionVIN;
 
-  tensionVOUT = 5.0F/1024.0F*tensionVOUT;
-  tensionVIN = tensionVOUT*((R1+R2)/R2);
+  tensionVOUT = 5.0F / 1024.0F * tensionVOUT;
+  tensionVIN = tensionVOUT * ((R1 + R2) / R2);
 
   return tensionVIN;
-
 }
 
 
@@ -30,33 +29,31 @@ float calculVin(float tensionVOUT){
 void setup() {
 
   Serial.begin(9600);
+  pinMode(PIN_MESURE_TENSION, INPUT);
   /** set up the LCD's number of columns and rows: */
   lcd.begin(16, 2);
-  /** Print a message to the LCD. */
+  /** Affichage des infos d'initialisation. */
   lcd.print("Orsys");
   lcd.setCursor(0, 1);
   lcd.print("Voltmetre");
   delay(2000);
 
+  /** Remise à zéro de l'écran */
   lcd.clear();
-  
-  pinMode(PIN_MESURE_TENSION, INPUT);
 
+  lcd.setCursor(0, 0);
+  lcd.print("Voltmetre");
 }
 
 void loop() {
 
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
   uint16_t mesureAnalogique = analogRead(PIN_MESURE_TENSION);
-  lcd.setCursor(0,0);
-  lcd.print("Voltmetre");
-  lcd.setCursor(0,1);
+  
+  /* Affichage HUD et tension */
+  lcd.setCursor(0, 1);
   lcd.print(calculVin(mesureAnalogique));
   lcd.print(" V");
-  
+
   // print the tension value:
   delay(500);
-  
 }
-

712b8fa0cd4cc777421087f4f30db55e92ce56cd
Alexandre <alexandre.sautet@protonmail.com>
2023-03-15 09:41:06 +0100

    Ajout chenillard + mod v2 pot

--- v2/v2.ino
+++ v2/v2.ino
@@ -1,37 +1,38 @@
-/*
-  Blink
+#define PIN_GREEN 4
+#define PIN_YELLOW 5
+#define PIN_RED 6
+#define PIN_BUTTON 2
+#define PIN_POT A0
+ 
+void setup() {
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+  Serial.begin(9600);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_YELLOW, OUTPUT);
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_POT, INPUT);
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+}
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+/**
+*Fonction du calcul du ratio via l'entree pot
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+float calculRatio(uint16_t valeurPOT, float vRef = 5.0F)
+{
+  float ratio =(vRef/1024.0F)*(float)valeurPOT;
+  return ratio;
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+
+  int buttonState = digitalRead(PIN_BUTTON);
+
+  int potValue = analogRead(PIN_POT);
+  Serial.println("Tension : ");
+  //Serial.println(potValue);
+  Serial.println(calculRatio(potValue));
+  delay(1000);
+
 }

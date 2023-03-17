a69370bbc7cfc4a2bf102a0da4c558d39a522317
Alexandre <alexandre.sautet@protonmail.com>
2023-03-17 13:39:30 +0100

    ajout_interruption

--- TFT_multi_display/TFT_multi_display.ino
+++ TFT_multi_display/TFT_multi_display.ino
@@ -5,8 +5,25 @@
 
 struct SensorsValues sensorsValues;
 
+#define INTERRUPT_PIN_2 2
+#define INTERRUPTION_LED 4
+
+// void actionInterruption(){
+//   digitalWrite(INTERRUPTION_LED, ! digitalRead(INTERRUPTION_LED));
+//   while (digitalRead(INTERRUPT_PIN_2) == LOW) {
+//   }
+// }
+
+volatile bool runloop = true;
+
+void actionInterruption() {
+  runloop = !runloop;
+}
 
 void setup() {
+
+  pinMode(INTERRUPTION_LED, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_2), actionInterruption, RISING);
 
 #ifdef OUTPUT_ACTIONNER
   setupOutput();
@@ -27,43 +44,46 @@ void setup() {
   delay(900);
   //tft.fillScreen(TFT_BACKGROUND_COLOR);
   readAndShowDatetime();
-  
 }
 
 void loop() {
-
-  readAndShowDatetime();
+  if (runloop) {
+    digitalWrite(INTERRUPTION_LED, HIGH);
+    readAndShowDatetime();
 #ifdef PRESSURE_SENSOR
-  sensorsValues.pressure = readPressure();
+    sensorsValues.pressure = readPressure();
 #endif
 #ifdef TEMPERATURE_SENSOR
-  sensorsValues.temperature = readTemperature();
+    sensorsValues.temperature = readTemperature();
 #endif
 #ifdef HUMIDITY_SENSOR
-  sensorsValues.humidity = readHumidity();
+    sensorsValues.humidity = readHumidity();
 #endif
 #ifdef OUTPUT_ACTIONNER
-  loopOutput();
+    loopOutput();
 #endif
 
-char result[7];
+    char result[7];
 
 #ifdef SDCARDINOUT
-    #ifdef HUMIDITY_SENSOR
-      ecrireText("data.csv",dtostrf(sensorsValues.humidity, 5, 2, result));
-    #endif
+#ifdef HUMIDITY_SENSOR
+    ecrireText("data.csv", dtostrf(sensorsValues.humidity, 5, 2, result));
+#endif
 
-    #ifdef TEMPERATURE_SENSOR
-      ecrireText("data.csv",dtostrf(sensorsValues.temperature, 5, 2, result));
-    #endif
+#ifdef TEMPERATURE_SENSOR
+    ecrireText("data.csv", dtostrf(sensorsValues.temperature, 5, 2, result));
+#endif
 
-    #ifdef PRESSURE_SENSOR
-      ecrireText("data.csv",dtostrf(sensorsValues.pressure, 5, 2, result));
-    #endif
+#ifdef PRESSURE_SENSOR
+    ecrireText("data.csv", dtostrf(sensorsValues.pressure, 5, 2, result));
+#endif
 
     ecrireRetourText("data.csv");
 #endif
+  }else
+  {
+    digitalWrite(INTERRUPTION_LED, LOW);
+  }
 
-  
   delay(1000);
 }
